807b6131989ce9c5f5c4920e35bd2729ff652f6f
Maciej Jarocki <mmaciej.jarocki@gmail.com>
2023-02-14 17:31:37 +0100

    New functionality added after the right lick element of the list is crossed out

--- shopping_list/src/Components/ShoppingList.js
+++ shopping_list/src/Components/ShoppingList.js
@@ -1,19 +1,25 @@
 import columnStyles from '../common/commonStyles.scss'
 
-const ShoppingList = ({products, handleRightClick}) => {
+const ShoppingList = ({products, handleLeftClickShoppingList, handleIsCrossedChange}) => {
+
     return (
         <div className={columnStyles.App}>
             <h1>Shopping list:</h1>
-            <ul>{products.map((product, idx) => 
-                <p 
-                    key={idx + 'S'} 
-                    onContextMenu={() => handleRightClick(product.name)}
-                    >{product.name}
-                </p>
-                )}
+            <ul>{products.map((product, idx) => {
+                return  <p key={idx}
+                            onClick={() => handleLeftClickShoppingList(idx)}
+                            style={(product.isCrossed) ? {textDecoration: 'line-through'} : null}
+                            onContextMenu={(e) => {
+                                e.preventDefault()
+                                handleIsCrossedChange(idx)
+                            }}
+                        >
+                            {product.name}
+                        </p>
+                })}
             </ul>
         </div>
-      )
+      ) 
 };
 
 export default ShoppingList;
